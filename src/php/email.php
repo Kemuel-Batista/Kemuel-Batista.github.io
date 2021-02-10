d043ef1cd479bc79c1f23210509040f77acd2fac
Kemuel Batista <kemuellima20@gmail.com>
2021-02-09 21:25:25 -0300

    Alteração 4.0

--- src/php/email.php
+++ src/php/email.php
@@ -1,46 +1,29 @@
 <?php
-if (isset($_POST['BTEnvia'])) {
- 
- //Variaveis de POST, Alterar somente se necessário 
- //====================================================
- $nome = $_POST['nome'];
- $email = $_POST['email'];
- $assunto = $_POST['assunto']; 
- $mensagem = $_POST['mensagem'];
- //====================================================
- 
- //REMETENTE --> ESTE EMAIL TEM QUE SER VALIDO DO DOMINIO
- //==================================================== 
- $email_remetente = $email; // deve ser uma conta de email do seu dominio 
- //====================================================
- 
- //Configurações do email, ajustar conforme necessidade
- //==================================================== 
- $email_destinatario = "klecyolegal2@gmail.com"; // pode ser qualquer email que receberá as mensagens
- $email_reply = "$email"; 
- $email_assunto = "$assunto"; // Este será o assunto da mensagem
- //====================================================
-  
- //Monta o Corpo da Mensagem
- //====================================================
- $email_conteudo = "Nome = $nome \n"; 
- $email_conteudo .= "Email = $email \n";
- $email_conteudo .= "assunto = $assunto \n";
- $email_conteudo .= "Mensagem = $mensagem \n"; 
- //====================================================
- 
- //Seta os Headers (Alterar somente caso necessario) 
- //==================================================== 
- $email_headers = implode ( "\n",array ( "From: $email_remetente", "Reply-To: $email_reply", "Return-Path: $email_remetente","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ) );
- //====================================================
- 
- //Enviando o email 
- //==================================================== 
- if (mail ($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers)){ 
- echo "</b>E-Mail enviado com sucesso!</b>"; 
- } 
- else{ 
- echo "</b>Falha no envio do E-Mail!</b>"; } 
- //====================================================
-} 
+//1 – Definimos Para quem vai ser enviado o email
+$para = "klecyolegal2@gmail.com";
+//2 - resgatar o nome digitado no formulário e  grava na variavel $nome
+$nome = $_POST['nome'];
+// 3 - resgatar o assunto digitado no formulário e  grava na variavel
+//$assunto
+$assunto = $_POST['assunto'];
+// obtendo email
+$email = $_POST['email'];
+ //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
+$mensagem = "<strong>Nome:  </strong>".$nome;
+$mensagem .= "<br>  <strong>Mensagem: </strong>"
+.$_POST['texto'];
+
+//5 – agora inserimos as codificações corretas e  tudo mais.
+$headers =  "Content-Type:text/html; charset=UTF-8\n";
+$headers .= "From:  {$nome}<{$email}>\n";
+//Vai ser //mostrado que  o email partiu deste email e seguido do nome
+$headers .= "X-Sender:  <{$email}>\n";
+//email do servidor //que enviou
+$headers .= "X-Mailer: PHP  v".phpversion()."\n";
+$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
+$headers .= "Return-Path:  <{$email}>\n";
+//caso a msg //seja respondida vai para  este email.
+$headers .= "MIME-Version: 1.0\n";
+
+mail($para, $assunto, $mensagem, $headers);  //função que faz o envio do email.
 ?>
--- src/php/sendMail.php
+++ src/php/sendMail.php
@@ -50,3 +50,4 @@ function SendEmail($message, $subject, $from, $to) {
 }
 
 ?>
+n
